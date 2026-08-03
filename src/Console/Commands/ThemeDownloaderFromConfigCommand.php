e2916ca5f0f653c7f4f74f8ba4fa0aa20ae94f1c
ArefShojaei <arefshojaei82@gmail.com>
2026-08-03 19:29:53 +0330

    refactor: update logic to use Theme processor

--- src/Console/Commands/ThemeDownloaderFromConfigCommand.php
+++ src/Console/Commands/ThemeDownloaderFromConfigCommand.php
@@ -2,13 +2,34 @@
 
 namespace App\Console\Commands;
 
+use Kit\Fs\File;
+use Kit\Json\Json;
+use PhpX\Utils\Console\Console;
 use PhpX\Components\Console\Command;
+
+use App\Components\Theme\ThemeProcessor;
 
 final class ThemeDownloaderFromConfigCommand extends Command
 {
     public function exec(array $params): string
     {
-        // TODO Implement logic code...
-        return "Testing...";
+        $configPath = dirname(__DIR__, 3) . "/theme.config.json";
+
+        if (!File::has($configPath)) {
+            return Console::error(
+                label: "VALIDATION",
+                message: "Theme config file doesn't exist!",
+            );
+        }
+
+        $content = File::get($configPath);
+
+        $themes = Json::decode($content, true);
+
+        $processor = new ThemeProcessor($themes);
+
+        $processor->process();
+
+        return "Done.";
     }
 }
